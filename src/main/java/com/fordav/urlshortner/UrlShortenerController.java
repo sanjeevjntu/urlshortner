b1b8cb1bb21c91090d79c229e0d7e1bc2b6f320e
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-01-30 02:10:46 -0500

    Initial commit for url shortner

---
+++ src/main/java/com/fordav/urlshortner/UrlShortenerController.java
@@ -0,0 +1,47 @@
+package com.fordav.urlshortner;
+
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class UrlShortenerController {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @GetMapping("/{id}")
+    public String getLongUrl(@PathVariable String id) {
+        String longUrl = redisTemplate.opsForValue().get(id);
+        log.info("Getting longUrl: {}", longUrl);
+        if (id == null) {
+            throw new RuntimeException("There is not Short Url is not found");
+        }
+        return longUrl;
+    }
+
+    @PostMapping("/createShortUrl")
+    public String createShortUrl(@RequestBody String longUrl) {
+        UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https"});
+
+        if (urlValidator.isValid(longUrl)) {
+            //Use murmur Hash3 to generate the short URL
+            String id = Hashing.murmur3_32()
+                    .hashString(longUrl, StandardCharsets.UTF_8)
+                    .toString();
+            log.info("ShortUrl: {}", id);
+            redisTemplate.opsForValue().set(id, longUrl);
+
+            return id;
+        }
+        throw new IllegalArgumentException(" long url is not valid");
+    }
+}
